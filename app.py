1533ec2ed1b3ae3928133797beaa111c0ae31d96
Hugo <62549984+HugoDrs@users.noreply.github.com>
2026-03-30 11:45:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ with st.expander("📖 Comment exporter le fichier depuis Airstock ?", expanded=
 
     col1, col2 = st.columns([1, 20])
     with col1: st.markdown("**7**")
-    with col2: st.markdown("Copie ton fichier dans le dossier **"Documents"** du bureau à distance puis colle le dans un dossier en local")
+    with col2: st.markdown("Copie ton fichier dans le dossier **« Documents »** du bureau à distance puis colle le dans un dossier en local")
 
 
 st.divider()
